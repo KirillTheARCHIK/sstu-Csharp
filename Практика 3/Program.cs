57032585b78b2ce3685df81b7b84ce9c464d517f
220887 <220887@K5-305-1685250>
2023-09-09 13:52:11 +0400

    1

---
+++ Практика 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практика_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
