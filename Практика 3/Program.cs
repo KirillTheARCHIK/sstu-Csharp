7532634e0dfac6509ab866b3b298f7f6f82dce56
220887 <220887@K5-305-1685250>
2023-09-09 14:04:27 +0400

    1

--- Практика 3/Program.cs
+++ Практика 3/Program.cs
@@ -6,7 +6,15 @@ namespace Практика_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите число: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.SetWindowSize(200, 100);
+            var dateTime = DateTime.Now;
+            Console.Write("{0}\n\n\n", dateTime.ToLongTimeString());
+            Console.WriteLine("{0:F2}".PadRight(20, '0'), n);
         }
     }
 }
