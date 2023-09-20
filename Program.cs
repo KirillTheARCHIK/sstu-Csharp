50f9bafe40abd0cbdc5d2a2740e83194e314ae90
220887 <220887@K5-305-1685250>
2023-09-20 14:19:00 +0400

    Добавьте файлы проекта.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Практики
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
