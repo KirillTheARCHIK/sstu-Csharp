8355e16017c1a290745e18933840d6a61bde2cdb
KirillTheARCHIK <kirillthearchik@yandex.ru>
2023-10-16 22:49:18 +0400

    1

--- Utils/BoolExpression.cs
+++ Utils/BoolExpression.cs
@@ -20,37 +20,143 @@ namespace Programs
                 int value = (index % (int)Math.Pow(2, variables.Count - j)) / (int)Math.Pow(2, variables.Count - j - 1);
                 variableValues.Add(variables.ElementAt(j), value == 1);
             }
+            if (BoolExpression.debug)
+            {
+                Console.WriteLine($"\nCASE   {String.Join(" ", variableValues.Values.Select(e => e ? "1" : "0"))}\n");
+            }
             result = ResolveExpression(expression);
         }
-        bool ResolveExpression(string subExpression)
+        string unaryOperators = "¬!";
+        string binaryOperators = "\\˄∧&\\|˅∨↓→=≡↔☺⊕";
+        //string variableChars = "[A-Za-z]{1}";
+        string variableChars = "[01]{1}";
+        bool ResolveExpression(string expression)
         {
-            if (subExpression.StartsWith("(") && subExpression.EndsWith(")"))
+            foreach (var entry in variableValues)
             {
-                subExpression = subExpression.Substring(1, subExpression.Length - 2);
+                expression = expression.Replace(entry.Key, entry.Value ? '1' : '0');
             }
-            if (subExpression.Length == 1)
+            while (expression.Length > 1)
             {
-                return variableValues[subExpression[0]];
+                if (BoolExpression.debug)
+                {
+                    Console.WriteLine(expression);
+                }
+                if (ReplaceBrackets(ref expression))
+                {
+                    continue;
+                }
+                if (ReplaceUnary(ref expression, "¬!┐".ToCharArray()))
+                {
+                    continue;
+                }
+                if (ReplaceBinary(ref expression, "˄∧&|".ToCharArray()))
+                {
+                    continue;
+                }
+                if (ReplaceBinary(ref expression, "˅∨↓".ToCharArray()))
+                {
+                    continue;
+                }
+                if (ReplaceBinary(ref expression, "→".ToCharArray()))
+                {
+                    continue;
+                }
+                if (ReplaceBinary(ref expression, "=≡↔☺⊕+".ToCharArray()))
+                {
+                    continue;
+                }
+                break;
             }
-            Regex binaryRegex = new Regex(@"\A(?<valueA>(\(.+\)|[A-Za-z]{1}))(?<operator>[\&\|])(?<valueB>(\(.+\)|[A-Za-z]{1}))\Z");
-            var binaryMatch = binaryRegex.Match(subExpression);
-            if (binaryMatch.Success)
+            if (BoolExpression.debug)
             {
-                return BoolOperation(ResolveExpression(binaryMatch.Groups["valueA"].Value), ResolveExpression(binaryMatch.Groups["valueB"].Value), binaryMatch.Groups["operator"].Value[0]);
+                Console.WriteLine(expression);
             }
-            Regex unaryRegex = new Regex(@"\A(?<operator>[\!])(?<value>(\(.+\)|[A-Za-z]{1}))\Z");
-            var unaryMatch = unaryRegex.Match(subExpression);
-            if (unaryMatch.Success)
+            if (expression.Length == 1)
             {
-                return BoolOperation(ResolveExpression(unaryMatch.Groups["value"].Value), unaryMatch.Groups["operator"].Value[0]);
+                return expression[0] == '1';
             }
             throw new OSExeption("Выражение составлено некорректно");
+            //if (subExpression.StartsWith("(") && subExpression.EndsWith(")"))
+            //{
+            //    subExpression = subExpression.Substring(1, subExpression.Length - 2);
+            //}
+            //if (subExpression.Length == 1)
+            //{
+            //    return variableValues[subExpression[0]];
+            //}
+            //Regex binaryRegex = new Regex($"\\A(?<valueA>(\\(.+\\)|{variableChars}))(?<operator>{binaryOperators})(?<valueB>(\\(.+\\)|{variableChars}))\\Z");
+            //var binaryMatch = binaryRegex.Match(subExpression);
+            //if (binaryMatch.Success)
+            //{
+            //    return BoolOperation(ResolveExpression(binaryMatch.Groups["valueA"].Value), ResolveExpression(binaryMatch.Groups["valueB"].Value), binaryMatch.Groups["operator"].Value[0]);
+            //}
+            //Regex unaryRegex = new Regex($"\\A(?<operator>{unaryOperators})(?<value>(\\(.+\\)|{variableChars}))\\Z");
+            //var unaryMatch = unaryRegex.Match(subExpression);
+            //if (unaryMatch.Success)
+            //{
+            //    return BoolOperation(ResolveExpression(unaryMatch.Groups["value"].Value), unaryMatch.Groups["operator"].Value[0]);
+            //}
+            //throw new OSExeption("Выражение составлено некорректно");
         }
+        bool ReplaceBrackets(ref string expression)
+        {
+            Regex regex = new Regex($"\\((?<valueA>{variableChars})\\)");
+            var match = regex.Match(expression);
+            if (match.Success)
+            {
+                expression = expression.Replace(match.Value, match.Groups["valueA"].Value);
+                return true;
+            }
+            return false;
+        }
+        bool ReplaceUnary(ref string expression, char[] operators)
+        {
+            Regex regex = new Regex($"(?<operator>[{String.Join("", operators)}])(?<valueA>{variableChars})");
+            var match = regex.Match(expression);
+            if (match.Success)
+            {
+                expression = expression.Replace(
+                    match.Value,
+                    BoolToChar(
+                        BoolOperation(
+                            StringToBool(match.Groups["valueA"].Value),
+                            match.Groups["operator"].Value[0]
+                        )
+                    ).ToString()
+                );
+                return true;
+            }
+            return false;
+        }
+        bool ReplaceBinary(ref string expression, char[] operators)
+        {
+            Regex regex = new Regex($"(?<valueA>{variableChars})(?<operator>[{String.Join("", operators)}])(?<valueB>{variableChars})");
+            var match = regex.Match(expression);
+            if (match.Success)
+            {
+                expression = expression.Replace(
+                    match.Value,
+                    BoolToChar(
+                        BoolOperation(
+                            StringToBool(match.Groups["valueA"].Value),
+                            StringToBool(match.Groups["valueB"].Value),
+                            match.Groups["operator"].Value[0]
+                        )
+                    ).ToString()
+                );
+                return true;
+            }
+            return false;
+        }
+        //Отрицание: ¬!
         bool BoolOperation(bool a, char operation)
         {
             switch (operation)
             {
                 case '!':
+                case '¬':
+                case '┐':
                     {
                         return !a;
                     }
@@ -58,26 +164,74 @@ namespace Programs
                     throw new OSExeption("Несуществующий унарный оператор");
             }
         }
+        //Коньюнкция: ∧˄&
+        //Дизъюнкция: ∨˅
+        //Импликация: → 
+        //Эквивалентность: =≡↔
+        //Исключающее или: ☺⊕
+        //Стрелка Пирса: ↓
+        //Штрих Шеффера: |
         bool BoolOperation(bool a, bool b, char operation)
         {
             switch (operation)
             {
+                case '∧':
+                case '˄':
                 case '&':
                     {
                         return a & b;
                     }
-                case '|':
+                case '∨':
+                case '˅':
                     {
                         return a | b;
+                    }
+                case '→':
+                    {
+                        return !a | b;
+                    }
+                case '=':
+                case '≡':
+                case '↔':
+                    {
+                        return a == b;
+                    }
+                case '⊕':
+                case '☺':
+                case '+':
+                    {
+
+                        return a != b;
+                    }
+                case '↓':
+                    {
+                        return !(a | b);
+                    }
+                case '|':
+                    {
+                        return !(a & b);
                     }
                 default:
                     throw new OSExeption("Несуществующий бинарный оператор");
             }
         }
+        static bool StringToBool(string str)
+        {
+            return str == "1" ? true : str == "0" ? false : throw new OSExeption("Неудачный перевод string в bool");
+        }
+        static bool CharToBool(char str)
+        {
+            return str == '1' ? true : str == '0' ? false : throw new OSExeption("Неудачный перевод char в bool");
+        }
+        static char BoolToChar(bool b)
+        {
+            return b ? '1' : '0';
+        }
     }
     internal class BoolExpression
     {
         static uint cellWidth = 3;
+        public static bool debug = true;
 
         HashSet<char> variables;
         List<BoolExpressionCase> cases;
