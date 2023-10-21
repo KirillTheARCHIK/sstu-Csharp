787d31c1289ef52fbde8f9dcb87eccaba84b95c3
220887 <220887@K5-305-1685250>
2023-10-21 13:02:25 +0400

    1

--- Programs/Practice8.cs
+++ Programs/Practice8.cs
@@ -30,20 +30,19 @@ namespace Programs
                 var ordered = words.OrderBy(x => x).ToList();
                 Console.WriteLine($"Отсортированная строка:\n{string.Join(" ", ordered)}");
             },
-            ["stat"] = delegate (Command command, History hist)
+            ["posts"] = delegate (Command command, History hist)
             {
                 var path = Path.GetFullPath("./../../") + "Utils\\MediumText.txt";
                 using (var stream = new StreamReader(path))
                 {
-                    var morph = new MorphAnalyzer(withLemmatization: true, maxBatchSize: 100000);
+                    var morph = new MorphAnalyzer(maxBatchSize: 100000);
 
                     var allText = stream.ReadToEnd();
-                    var allWords = allText.Split(new char[] { '.', '!', '?', ':', ';', ' ', ',', '\"', '\'', '\n', '\r', '\t', '—' }, StringSplitOptions.RemoveEmptyEntries);
                     var sentenses = allText.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-
                     //var results = morph.Parse(allWords).ToArray();
                     //var morphInfo = results[0];
                     //Console.WriteLine(String.Join("\n", morphInfo["чр"].Grams));
+
                     List<int> columnWidths = new List<int>() { 4 };
                     List<string> postNames = new List<string>() {
                         "част",
@@ -71,9 +70,20 @@ namespace Programs
                         {
                             continue;
                         }
-                        var morphInfo = results[0];
-                        var morphGrams = morphInfo["чр"].Grams.ToDictionary<Gram, string, double>(gram => gram.Key, gram => gram.Power);
                         var row = new List<string>() { (i + 1).ToString() };
+                        var morphGrams = new Dictionary<string, double>();
+                        foreach (var morphInfo in results)
+                        {
+                            var bestGram = morphInfo["чр"].BestGram;
+                            if (morphGrams.ContainsKey(bestGram.Key))
+                            {
+                                morphGrams[bestGram.Key]++;
+                            }
+                            else
+                            {
+                                morphGrams.Add(bestGram.Key, bestGram.Power);
+                            }
+                        }
                         foreach (var postName in postNames)
                         {
                             double power;
@@ -81,15 +91,49 @@ namespace Programs
                             {
                                 power = 0;
                             }
-                            row.Add(((int)(power * words.Length)).ToString());
+                            row.Add((Math.Ceiling(power)).ToString());
                         }
                         postValues.Add(row);
+                        //var morphInfo = results[0];
                     }
                     postNames.Insert(0, "№ пр");
                     ConsoleTable.makeTable(columnWidths, postNames, postValues);
-
-                    
                 }
+            },
+            ["words"] = delegate (Command command, History hist)
+            {
+                var path = Path.GetFullPath("./../../") + "Utils\\MediumText.txt";
+                string allText;
+                using (var stream = new StreamReader(path))
+                {
+
+                    allText = stream.ReadToEnd();
+                }
+                var morph = new MorphAnalyzer(withLemmatization: true, maxBatchSize: 1000000);
+                var allWords = allText.Split(new char[] { '.', '!', '?', ':', ';', ' ', ',', '\"', '\'', '\n', '\r', '\t', '—' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(allWords.Length.ToString());
+                var result = morph.Parse(allWords.Take(1500)).ToArray();
+                var lemmas = new Dictionary<string, int>();
+                foreach (var morphInfo in result)
+                {
+                    if (morphInfo.BestTag.HasLemma)
+                    {
+                        var lemma = morphInfo.BestTag.Lemma;
+                        if (lemmas.ContainsKey(lemma))
+                        {
+                            lemmas[lemma]++;
+                        }
+                        else
+                        {
+                            lemmas.Add(lemma, 1);
+                        }
+                    }
+                }
+                ConsoleTable.makeTable(
+                    new List<int> { lemmas.Max(lemma => lemma.Key.Length), 4 },
+                    new List<string> { "Лемма", "Кол." },
+                    lemmas.OrderByDescending(lemma => lemma.Value).Select(lemma => new List<string> { lemma.Key, lemma.Value.ToString() })
+                );
             }
         })
         { }
